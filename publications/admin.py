4104a0664252a4eefab6d8b54ec62b4a9c5a9e83
Daniel Nüst <daniel.nuest@tu-dresden.de>
2023-11-07 00:38:01 +0100

    add bulk actions to admin backend

--- publications/admin.py
+++ publications/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin
 from publications.models import Publication
+from import_export.admin import ImportExportModelAdmin
+
+@admin.action(description="Mark selected publications as published")
+def make_public(modeladmin, request, queryset):
+    queryset.update(status="p")
+
+@admin.action(description="Mark selected publications as draft (unpublished)")
+def make_draft(modeladmin, request, queryset):
+    queryset.update(status="d")
 
 @admin.register(Publication)
 class PublicationAdmin(LeafletGeoAdmin, ImportExportModelAdmin):
