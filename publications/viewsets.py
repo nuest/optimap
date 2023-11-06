335592afc6f6f9df1253b9102bc7f987e5162baf
Daniel Nüst <daniel.nuest@tu-dresden.de>
2023-11-07 00:34:07 +0100

    only show published publications

--- publications/viewsets.py
+++ publications/viewsets.py
@@ -7,18 +7,18 @@ import requests
 
 class PublicationViewSet(viewsets.ReadOnlyModelViewSet):
     """publication view set."""
-   
+
     bbox_filter_field = "location"
-    filter_backends = (filters.InBBoxFilter,)   
-    queryset = Publication.objects.all()
+    filter_backends = (filters.InBBoxFilter,)
+    queryset = Publication.objects.all().filter(status="p")
     serializer_class = PublicationSerializer
 
 class SubscriptionViewset(viewsets.ModelViewSet):
 
     bbox_filter_field = "location"
-    filter_backends = (filters.InBBoxFilter,)   
+    filter_backends = (filters.InBBoxFilter,)
     serializer_class = SubscriptionSerializer
-       
+
     def get_queryset(self):
         user = self.request.user
         queryset = Subscription.objects.filter(user_name=user)
