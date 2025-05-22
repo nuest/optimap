2b4f314b68b1e043060474c3a733dd032a33bedc
Daniel Nüst <daniel.nuest@tu-dresden.de>
2025-05-22 13:35:58 +0200

    update compose configurations, use entrypoint script (for now)

---
+++ etc/manage-and-run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# Collect static files
+echo "OPTIMAP ENTRYPOINT | Collect static files"
+python manage.py collectstatic --noinput
+
+# Create a cache table
+echo "OPTIMAP ENTRYPOINT | Create cache table"
+python manage.py createcachetable
+
+# Apply database migrations
+echo "OPTIMAP ENTRYPOINT | Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "OPTIMAP ENTRYPOINT | Starting server"
+# make the next process the main process, cf. https://www.baeldung.com/ops/docker-exec-process-replacement
+exec python manage.py runserver 0.0.0.0:8000
+
+# TODO: revisit entrypoint approach if there are any issues, e.g., by using an extra migration container, cf. https://stackoverflow.com/questions/33992867/how-do-you-perform-django-database-migrations-when-using-docker-compose, or by manually running selected commands after installation, cf. https://www.baeldung.com/ops/django-database-migrations-docker-compose
